c2835fae0db8e119fb371b7fa59f2ebcf68b84dc
ACER <sumit.jaiswal8888@gmail.com>
2025-10-27 19:38:59 +0530

    adding configuration file

---
+++ src/main/java/com/example/Scholarship_matcher/configurations/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.example.Scholarship_matcher.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://scholarship-matcher-frontend.vercel.app/", // 👈 your actual frontend URL
+                                "http://localhost:5173" // for local testing
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
